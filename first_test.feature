950d31629910f31c8dda0f0a1734cca254b3922b
mediasoft <mediasoft@Mac-mini-mediasoft-4.local>
2021-05-13 14:42:35 +0400

    upd

--- first_test.feature
+++ first_test.feature
@@ -4,5 +4,5 @@ Feature: Checking search
   Scenario: Сheck some text in search results
 #И используем наши шаги.
     Given website "ya.ru"
-    Then push button with text 'Найти'
+    When push button with text 'Найти'
     Then page include text 'Задан пустой поисковый запрос'
--- steps/first_test.py
+++ steps/first_test.py
@@ -9,18 +9,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 @given('website "{url}"')
 def step(context, url):
     # Измените строку, для выполнения теста в другом браузере
-    context.browser = webdriver.Firefox()
+    context.browser = webdriver.Chrome()
     context.browser.maximize_window()
     context.browser.get("http://ya.ru")
 
 
 # Теперь нажмем на кнопку "Найти"
-@then("push button with text '{text}'")
+@when("push button with text '{text}'")
 def step(context, text):
     WebDriverWait(context.browser, 120).until(
         EC.element_to_be_clickable((By.XPATH, '//button'))
     )
-    context.browser.find_element_by_xpath('//button').click()
+
+    context.browser.find_element(By.XPATH, '//button').click()
 
 
 # Проверим, что мы на странице с результатами поиска, есть некоторый искомый текст
@@ -29,5 +30,6 @@ def step(context, text):
     WebDriverWait(context.browser, 120).until(
         EC.presence_of_element_located((By.XPATH, '//*[contains(text(), "%s")]' % text))
     )
-    assert context.browser.find_element_by_xpath('//*[contains(text(), "%s")]' % text)
+    assert context.browser.find_element(By.XPATH, '//*[contains(text(), "%s")]' % text)
+
     context.browser.quit()
