3f406c6e4577ae7193e9ab1b9ed863b5e50b0612
mediasoft <mediasoft@Mac-mini-mediasoft-4.local>
2021-05-17 16:40:30 +0400

    clean code

--- steps/first_test.py
+++ steps/first_test.py
@@ -16,16 +16,14 @@ def step(context, url):
 # Теперь нажмем на кнопку "Найти"
 @when("push button with text '{text}'")
 def step(context, text):
-    WebDriverWait(context.browser, 120).until(
-        EC.element_to_be_clickable((By.XPATH, '//button')))
-    context.browser.find_element(By.XPATH, '//button').click()
+    WebDriverWait(context.browser, 3).until(EC.element_to_be_clickable((By.XPATH, '//button'))).click()
 
 
-# Проверим, что мы на странице с результатами поиска, есть некоторый искомый текст
+# Проверим, что на странице с результатами поиска есть искомый текст
 @then("page include text '{text}'")
 def step(context, text):
-    WebDriverWait(context.browser, 120).until(
-        EC.presence_of_element_located((By.XPATH, '//*[contains(text(), "%s")]' % text)))
-    assert context.browser.find_element(By.XPATH, '//*[contains(text(), "%s")]' % text)
+    WebDriverWait(context.browser, 3).until(
+        EC.presence_of_element_located((By.XPATH, f'//*[contains(text(), "{text}")]')))
+    assert context.browser.find_element(By.XPATH, f'//*[contains(text(), "{text}")]')
 
     context.browser.quit()
