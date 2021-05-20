796bdab89862690863234afb2ff722a912bbaaf0
mediasoft <666hate666>
2021-05-20 15:36:22 +0400

    update

---
+++ search_google_test.feature
@@ -0,0 +1,16 @@
+Feature: Google Searching
+  As a web surfer, I want to search Google, so that I can learn new things.
+  
+# noinspection CucumberUndefinedStep
+  @automated @google @panda
+  Scenario Outline: Simple Google searches
+    Given a web browser is on the Google page
+    When the search phrase '<phrase>' is entered
+    And the button 'Поиск в Google' is clicked
+    Then results for '<phrase>' are shown
+    And the related results include '<related>'
+
+    Examples: Animals
+      | phrase   | related                |
+      | panda    | Ailuropoda melanoleuca |
+      | elephant | The White Stripes      |
---
+++ steps/search_google_test.py
@@ -0,0 +1,41 @@
+from behave import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+# Откроем главную страницу. Передадим в качестве аргумента адрес страницы.
+@given("a web browser is on the Google page")
+def step(context):
+    context.browser = webdriver.Chrome()
+    context.browser.maximize_window()
+    context.browser.get("https://google.com")
+
+
+# Введем поисковый запрос "Тест"
+@when("the search phrase '{phrase}' is entered")
+def step(context, phrase):
+    context.browser.find_element(By.XPATH, "//input[@name='q']").send_keys(f'{phrase}')
+
+
+# Теперь нажмем на кнопку
+@when("the button 'Поиск в Google' is clicked")
+def step(context):
+    button = WebDriverWait(context.browser, 3).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='btnK']")))
+    button.click()
+
+
+# Проверим, что на странице с результатами поиска есть искомый текст
+@then("results for '{phrase}' are shown")
+def step(context, phrase):
+    WebDriverWait(context.browser, 3).until(
+        EC.presence_of_element_located((By.XPATH, f"//*[contains(text(), '{phrase}')]")))
+    context.browser.quit()
+
+
+@then("the related results include '{related}'")
+def step(context, related):
+    WebDriverWait(context.browser, 3).until(
+        EC.presence_of_element_located((By.XPATH, f"//*[contains(text(), '{related}')]")))
+    context.browser.quit()
